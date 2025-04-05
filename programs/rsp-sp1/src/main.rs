a2915d00b3a64bc1b5dcaf2a0516f5dac6cd17ef
kaiwei-0 <kaiwei@brevis.network>
2025-04-05 11:04:05 +0800

    update rsp

--- programs/rsp-sp1/src/main.rs
+++ programs/rsp-sp1/src/main.rs
@@ -1,18 +1,27 @@
 #![no_main]
-
-use rsp_client_executor::{io::ClientExecutorInput, ClientExecutor, EthereumVariant};
-
 sp1_zkvm::entrypoint!(main);
+
+use rsp_client_executor::{
+    executor::{EthClientExecutor, DESERIALZE_INPUTS},
+    io::EthClientExecutorInput,
+};
+use std::sync::Arc;
 
 pub fn main() {
     // Read the input.
-    let input: Vec<u8> = sp1_zkvm::io::read_vec();
-    let input = bincode::deserialize::<ClientExecutorInput>(&input).unwrap();
+    println!("cycle-tracker-report-start: {}", DESERIALZE_INPUTS);
+    let input = sp1_zkvm::io::read_vec();
+    let input = bincode::deserialize::<EthClientExecutorInput>(&input).unwrap();
+    println!("cycle-tracker-report-end: {}", DESERIALZE_INPUTS);
 
     // Execute the block.
-    let executor = ClientExecutor;
-    let header = executor.execute::<EthereumVariant>(input).expect("failed to execute client");
+    let executor = EthClientExecutor::eth(
+        Arc::new((&input.genesis).try_into().unwrap()),
+        input.custom_beneficiary,
+    );
+    let header = executor.execute(input).expect("failed to execute client");
     let block_hash = header.hash_slow();
 
-    println!("block_hash: {:?}", block_hash);
+    // Commit the block hash.
+    sp1_zkvm::io::commit(&block_hash);
 }
