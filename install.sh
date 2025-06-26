5d2dabdfb989152768eae755ac36607c8c3d71b1
zktommy <tommy@brevis.network>
2025-06-26 10:52:43 +0800

    chore: revert changes for bench scripts

--- install.sh
+++ install.sh
@@ -10,8 +10,8 @@ error_exit() {
 }
 
 # Install Rust
-# curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y || error_exit "Installing Rust"
-# source $HOME/.cargo/env
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y || error_exit "Installing Rust"
+source $HOME/.cargo/env
 
 # Install the nightly toolchain
 rustup toolchain install nightly --profile minimal -c rust-src || error_exit "Installing nightly toolchain"
@@ -20,10 +20,10 @@ rustup toolchain install nightly --profile minimal -c rust-src || error_exit "In
 rustup default nightly || error_exit "Setting default toolchain to nightly"
 
 # Install the Succinct toolchain
-# curl -L https://sp1.succinct.xyz | bash || error_exit "Installing Succinct toolchain"
-# export PATH="$PATH:$HOME/.sp1/bin"
-# sp1up || error_exit "Updating Succinct toolchain"
-# cargo prove --version || error_exit "Checking cargo prove version"
+curl -L https://sp1.succinct.xyz | bash || error_exit "Installing Succinct toolchain"
+export PATH="$PATH:$HOME/.sp1/bin"
+sp1up || error_exit "Updating Succinct toolchain"
+cargo prove --version || error_exit "Checking cargo prove version"
 
 # Install the Risc0 toolchain
 curl -L https://risczero.com/install | grep -v 'set -euo pipefail' | bash || error_exit "Installing Risc Zero toolchain"
--- install_rzup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-git clone https://github.com/risc0/risc0.git
-cd riscv0
-git switch --detach v3.0.0-rc.1
-cargo uninstall rzup
-cargo run --bin rzup install
